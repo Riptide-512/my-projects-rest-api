ef3fabe579f8116841a6054a19ab5aaf2e98c781
Riptide-512 <eric.ivan.sepulveda@gmail.com>
2026-03-13 22:22:34 -0500

    added eslint and prettier

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import { Hono } from "hono";
+import { Hono } from 'hono'
 
-const app = new Hono();
+const app = new Hono()
 
-app.get("/", (c) => {
-  return c.json({ msg: "Hello Hono!" });
-});
+app.get('/', (c) => {
+  return c.json({ msg: 'Hello Hono!' })
+})
 
-export default app;
+export default app
